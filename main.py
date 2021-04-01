3549e3bbdd64534b63beafb4e5ef86c16309536d
Nayif <76570869+nayvd@users.noreply.github.com>
2021-04-01 03:48:39 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+running = True
 name = 'Enter a name in this variable'
 
 print('Welcome to ' + name + ', lets get you all set up, create you an acount, with a password and a username!')
@@ -13,11 +14,18 @@ password = setup_password
 if password == username:
     print('Password and username cannot match.')
 
-retype_password = input('Retype your password: ')
-if retype_password == password:
-    print('Successfully made account: ' + username)
-
 else:
-    print('Passwords do not match.')
-    
+    retype_password = input('Retype your password: ')
+    if retype_password == password:
+        print('Successfully made account: ' + username)
+    if password == username:
+        running = False
+    elif password == username:
+        running = True
+    if running == False:
+        print(' ')
+        running == True
+    if running == True:
+        print()
+        
 print('Enjoyed this project? Well thanks! Follow me on GitHub Link: [https://github.com/https://github.com/nayvd] Thanks!')
